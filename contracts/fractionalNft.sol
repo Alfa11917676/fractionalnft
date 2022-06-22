745dc2a4713c9404cdf20b60c6b3b5fe89d72a19
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2022-06-22 09:35:19 +0530

    Code Updated

--- contracts/fractionalNft.sol
+++ contracts/fractionalNft.sol
@@ -1,20 +1,28 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+pragma solidity ^0.8.7;
+import "./utils/ERC721Upgradeable.sol";
+import "./utils/Ownable.sol";
+import './utils/Pausable.sol';
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./fractionalSigner.sol";
-contract fractionalNft is Ownable, ERC721,SignerImplementation, ReentrancyGuard {
+contract fractionalNft is Ownable, Pausable, ERC721Upgradeable, SignerImplementation, ReentrancyGuardUpgradeable {
 
     mapping (address => mapping (uint => bool)) public nonceStatus;
     bool public isRaffleDone;
     uint public currentTokenId = 1;
     address public designatedSigner;
-    constructor (string memory _name, string memory _symbol) ERC721(_name,_symbol){}
 
+    function init (string memory _name, string memory _symbol, string memory _signer, string memory _version) external initializer {
+               Ownableinitialize();
+               Pausableinitialize();
+               __ReentrancyGuard_init();
+               __ERC721_init(_name,_symbol);
+               __Eighty80StakingContractSigner_init(_signer,_version);
+    }
 
-    function mintTokens(Signer memory signer) external nonReentrant{
+    function mintTokens(Signer memory signer) external nonReentrant {
         require (getSigner(signer) == designatedSigner,'!Signer');
         require(msg.sender == signer._user, '!User');
         require (block.timestamp <= signer.nonce + 10 minutes, 'Signature Expired');
@@ -25,6 +33,12 @@ contract fractionalNft is Ownable, ERC721,SignerImplementation, ReentrancyGuard 
             _mint(msg.sender,currentTokenId);
         }
     }
+
+    function addSigner(address _signer) external onlyOwner {
+        designatedSigner = _signer;
+    }
+
+
 
     function _beforeTokenTransfer(
         address from,
--- contracts/fractionalSigner.sol
+++ contracts/fractionalSigner.sol
@@ -1,13 +1,11 @@
 //SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+///@author Alfa
+contract SignerImplementation is EIP712Upgradeable{
 
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-
-contract SignerImplementation is EIP712{
-
-    string private constant SIGNING_DOMAIN = "FRACTIONAL";
-    string private constant SIGNATURE_VERSION =  "1";
+    string private SIGNING_DOMAIN;
+    string private SIGNATURE_VERSION;
 
     struct Signer{
         address _user;
@@ -16,7 +14,11 @@ contract SignerImplementation is EIP712{
         bytes signature;
     }
 
-  constructor() EIP712(SIGNING_DOMAIN,SIGNATURE_VERSION) {}
+    function __Eighty80StakingContractSigner_init(string memory domain, string memory version) internal initializer {
+        SIGNATURE_VERSION = version;
+        SIGNING_DOMAIN = domain;
+        __EIP712_init(domain, version);
+    }
 
     function getSigner(Signer memory signer) public view returns(address){
         return _verify(signer);
@@ -27,15 +29,15 @@ contract SignerImplementation is EIP712{
     function _hash(Signer memory signer) internal view returns (bytes32) {
         return _hashTypedDataV4(keccak256(abi.encode(
                 keccak256("Signer(address _user,uint256 totalTokensToMint,uint256 nonce)"),
-                signer._user,
-                signer.totalTokensToMint,
-                signer.nonce
+                    signer._user,
+                    signer.totalTokensToMint,
+                    signer.nonce
             )));
     }
 
     function _verify(Signer memory signer) internal view returns (address) {
         bytes32 digest = _hash(signer);
-        return ECDSA.recover(digest, signer.signature);
+        return ECDSAUpgradeable.recover(digest, signer.signature);
     }
 
 }
