e8e73a9c9f41631ac34b66ee1cc4aebed392ba71
0xAnyx <97167227+0xAlfa@users.noreply.github.com>
2022-06-23 17:34:31 +0530

    Code Updated To ERC1155Upgradeable.sol and also included the upgraded version of chainlinkVrf V2 and included the Raffle

--- contracts/fractionalNft.sol
+++ contracts/fractionalNft.sol
@@ -2,27 +2,50 @@
 
 
 pragma solidity ^0.8.7;
-import "./utils/ERC721Upgradeable.sol";
+import "./utils/ERC1155Upgradeable.sol";
 import "./utils/Ownable.sol";
 import './utils/Pausable.sol';
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./fractionalSigner.sol";
-contract fractionalNft is Ownable, Pausable, ERC721Upgradeable, SignerImplementation, ReentrancyGuardUpgradeable {
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
+
+contract fractionalNft is Ownable, Pausable, ERC1155Upgradeable, SignerImplementation, ReentrancyGuardUpgradeable, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
     mapping (address => mapping (uint => bool)) public nonceStatus;
+    mapping (uint => address) public ownerOfToken;
     bool public isRaffleDone;
     uint public currentTokenId = 1;
     address public designatedSigner;
+    uint public _maxSupply;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    mapping (address => bool) public validUsers;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    address s_owner;
 
-    function init (string memory _name, string memory _symbol, string memory _signer, string memory _version) external initializer {
+
+    function init (string memory _uri, string memory _signer, string memory _version, uint64 subscriptionId, address _vrfAddress, address _vrfCoordinator, address _marketPlaceAddress, uint _supply) external initializer {
                Ownableinitialize();
                Pausableinitialize();
+               __VRFConsumerBaseV2_init(_vrfAddress);
+               COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+                validUsers[_marketPlaceAddress] = true;
+               _maxSupply = _supply;
+               s_owner = msg.sender;
+               s_subscriptionId = subscriptionId;
                __ReentrancyGuard_init();
-               __ERC721_init(_name,_symbol);
+               __ERC1155_init(_uri);
                __Eighty80StakingContractSigner_init(_signer,_version);
     }
 
     function mintTokens(Signer memory signer) external nonReentrant {
+        require (validUsers[msg.sender],'!Valid User Call');
         require (getSigner(signer) == designatedSigner,'!Signer');
         require(msg.sender == signer._user, '!User');
         require (block.timestamp <= signer.nonce + 10 minutes, 'Signature Expired');
@@ -30,7 +53,7 @@ contract fractionalNft is Ownable, Pausable, ERC721Upgradeable, SignerImplementa
         nonceStatus[msg.sender][signer.nonce] = true;
         for (uint i =0;i<signer.totalTokensToMint;i++) {
             currentTokenId++;
-            _mint(msg.sender,currentTokenId);
+            _mint(msg.sender,currentTokenId,1,'');
         }
     }
 
@@ -38,18 +61,49 @@ contract fractionalNft is Ownable, Pausable, ERC721Upgradeable, SignerImplementa
         designatedSigner = _signer;
     }
 
+    function addValidUser(address _user) external onlyOwner {
+        validUsers[_user] = true;
+    }
 
+    function startRaffle() external onlyOwner {
+        require (validUsers[msg.sender],'!Valid User');
+        require (currentTokenId > _maxSupply/2,'50% not reached');
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            1
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 ,
+        uint256[] memory randomWords
+    ) internal override {
+        isRaffleDone = true;
+        uint randomNumber = randomWords[0];
+        uint winnerTokenId;
+        winnerTokenId = randomNumber % currentTokenId;
+        winnerTokenId +=2;
+        _mint(ownerOfToken[winnerTokenId],1,1,'');
+    }
 
     function _beforeTokenTransfer(
+        address operator,
         address from,
         address to,
-        uint256 tokenId
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
     ) internal override virtual {
-        require (!isRaffleDone || tokenId == 1,'Transfer| Stopped');
+
+        for (uint i=0;i<ids.length;i++) {
+            require(!isRaffleDone || ids[i] == 1, 'Transfer| Stopped');
+            ownerOfToken[ids[i]] = to;
+        }
         super._beforeTokenTransfer(
-         from,
-         to,
-        tokenId);
+            operator, from, to, ids, amounts, data);
     }
 
 }
