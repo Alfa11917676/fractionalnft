5e2098c37e6a5e5058695fff65247c8b51b2998c
0xAnyx <alfatitas747ray@outlook.com>
2022-06-25 09:02:22 +0530

    NFT code converted to chainlink V1

--- contracts/fractionalNft.sol
+++ contracts/fractionalNft.sol
@@ -8,14 +8,13 @@ import './utils/Pausable.sol';
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "./fractionalSigner.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-
-contract fractionalNft is Ownable, Pausable, ERC1155Upgradeable, SignerImplementation, ReentrancyGuardUpgradeable, VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface COORDINATOR;
-    uint64 public s_subscriptionId;
-    address public vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
-    bytes32 public keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+contract fractionalNft is Ownable, Pausable, ERC1155Upgradeable, SignerImplementation, ReentrancyGuardUpgradeable, VRFConsumerBase {
+//    VRFCoordinatorV2Interface COORDINATOR;
+//    uint64 public s_subscriptionId;
+//    address public vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 public keyHash;
     mapping (address => mapping (uint => bool)) public nonceStatus;
     mapping (uint => address) public ownerOfToken;
     bool public isRaffleDone;
@@ -23,27 +22,30 @@ contract fractionalNft is Ownable, Pausable, ERC1155Upgradeable, SignerImplement
     address public designatedSigner;
     uint public _maxSupply;
     uint public currentSupply;
-    uint32 callbackGasLimit = 100000;
-    uint16 requestConfirmations = 3;
+//    uint32 callbackGasLimit = 100000;
+//    uint16 requestConfirmations = 3;
     mapping (address => bool) public validUsers;
-    uint256[] public s_randomWords;
-    uint256 public s_requestId;
-    address s_owner;
+//    uint256[] public s_randomWords;
+//    uint256 public s_requestId;
+//    address s_owner;
     bool public onlyOnce;
+//    bytes32  internal keyHash;
+    uint256  internal fee;
 
-
-    function init (string memory _uri, string memory _signer, string memory _version, uint64 subscriptionId, address _vrfAddress, address _vrfCoordinator, address _marketPlaceAddress, uint _supply) external  {
+    function init (string memory _uri, string memory _signer, string memory _version,address _vrfCoordinator, address linkAddress, address _marketPlaceAddress, uint _supply) external  {
                 require (!onlyOnce,'Already Initialised');
                 onlyOnce = true;
                Ownableinitialize();
                Pausableinitialize();
-               __VRFConsumerBaseV2_init(_vrfAddress);
-               COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
-                keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+               __VRFConsumerBaseV1_init(_vrfCoordinator, linkAddress);
+//               COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+//                keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
                 validUsers[_marketPlaceAddress] = true;
                _maxSupply = _supply;
-               s_owner = msg.sender;
-               s_subscriptionId = subscriptionId;
+//                __VRFInit_(0x8C7382F9D8f56b33781fE506E897a4F1e2d17255,0x326C977E6efc84E512bB9C30f76E30c160eD06FB); //for matic
+                 keyHash = 0x6e75b569a01ef56d18cab6a8e71e6600d6ce853834d4a5748b720d06f878b3a4;
+//               s_owner = msg.sender;
+//               s_subscriptionId = subscriptionId;
                __ReentrancyGuard_init();
                __ERC1155_init(_uri);
                __Eighty80StakingContractSigner_init(_signer,_version);
@@ -78,34 +80,24 @@ contract fractionalNft is Ownable, Pausable, ERC1155Upgradeable, SignerImplement
         validUsers[_user] = true;
     }
 
-    function startRaffle() external onlyOwner {
+    function startRaffle() external onlyOwner returns(bytes32){
         require (validUsers[msg.sender],'!Valid User');
         require (currentTokenId - 1 > _maxSupply/2,'50% not reached');
-        s_requestId = COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            1
-        );
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        require (!isRaffleDone,'Raffle Ended');
+        bytes32 data = requestRandomness(keyHash, fee);
+        return data;
     }
 
-    function fulfillRandomWords(
-        uint256 ,
-        uint256[] memory randomWords
+    function fulfillRandomness(
+        bytes32 , uint256 randomness
     ) internal override {
         isRaffleDone = true;
-        uint randomNumber = randomWords[0];
         uint winnerTokenId;
-        winnerTokenId = randomNumber % currentTokenId;
+        winnerTokenId = randomness % currentTokenId;
         winnerTokenId +=2;
         _mint(ownerOfToken[winnerTokenId],1,1,'');
     }
-
-    function changeSubscriptionId(uint64 _id) external onlyOwner {
-        s_subscriptionId = _id;
-    }
-
 
     function _beforeTokenTransfer(
         address operator,
